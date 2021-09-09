e8bf8c5eb4426a16f99320ecb80967d0dd2e40f3
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-09-09 23:58:37 +1100

    Array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,21 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
     result = arr1.every((a, index) => a === arr2[index]) &&
       arr1.length === arr2.length
 
-    return result; // boolean
-
+    return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
   let result = arr.filter(item => item > 0 && item % 3 === 0);
       resultArr = result.map(item => item * 10);
-     return resultArr; // array
-
+     return resultArr;
 }
