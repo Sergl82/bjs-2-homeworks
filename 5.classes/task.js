6f1f137c0f9149f293ac659aa4fb9c3dd4b1fae5
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-09-23 21:53:41 +1100

    Classes

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,9 +1,7 @@
-
 class PrintEditionItem {
-
-  constructor (name, releaseData, pagesCount) {
+  constructor (name, releaseDate, pagesCount) {
     this.name = name;
-    this.releaseDate = releaseData;
+    this.releaseDate = releaseDate;
     this.pagesCount = pagesCount;
     this.state = 100;
     this.type = null;
@@ -81,23 +79,87 @@ class Library {
   }
 
   addBook (book) {
-    this.books.push(book)
+    if (book.state >= 30){
+      this.books.push(book)
+    }
     return this.books;
   }
 
   findBookBy (type, value) {
-  for (let i = 0; i < this.books.length; i++) {
-    if (this.books[i][type] === value) {
-      return this.books[i];
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i][type] === value) {
+        return this.books[i];
+      }
     }
-  }
-  return null
+    return null
 
+  }
+
+  giveBookByName (bookName) {
+    let bookSearch = [];
+    for (let key in this.books) {
+      if(this.books[key].name === bookName) {
+        bookSearch.push(this.books[key])
+        this.books.splice(bookSearch, 1)
+      }
+    }
+    return bookSearch;
+  }
+
+   }
+
+//задача 2
+
+class Student {
+  // ваш код
+  constructor (name, journal) {
+    this.name = name;
+    this.journal = [];
+
+  }
+
+  addMark(mark, subject) {
+   if (mark < 6 && mark > 0) {
+     this.journal.push({ subject, mark });
+     return this.journal;
+   }else
+     return  "Ошибка, оценка должна быть числом от 1 до 5";
+  }
+
+  getAverageBySubject(subject) {
+    let sum = [];
+    let average;
+    this.journal.filter((item) => {
+      if (item.subject === subject) {
+        sum.push(item.mark)
+        average = sum.reduce((a, b) => a + b) / sum.length;
+      }
+    return 'Несуществующий предмет';
+    });
+    return average;
+  }
+
+  getAverage(){
+    let sum = [];
+    let average;
+    this.journal.filter((item) => sum.push(item.mark))
+     return  sum.reduce((a, b) => a + b) / sum.length;
+  }
+
+  exclude(){
+    delete this.journal.subject;
+    return "Исключен за попытку подделать оценки";
+  }
 }
-  }
 
-  const library = new Library('Библиотека имени Ленина')
-  library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008))
-  library.addBook(new FantasticBook('Аркадий и Борис Стругацкие', 'Пикник на обочине', 1972, 168, state = 30));
-  library.findBookBy('releaseDate', 1900)
-console.log(library.findBookBy())
+const student = new Student("Олег Никифоров" );
+student.addMark(3, "algebra");
+student.addMark(5, "algebra");
+student.addMark(5, "geometry");
+student.addMark(4, "geometry");
+student.addMark(6, "geometry"); // "Ошибка, оценка должна быть числом от 1 до 5"
+student.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
+student.getAverageBySubject("biology"); // Несуществующий предмет
+student.getAverage(); // Средний балл по всем предметам 4.75
+student.exclude("Исключен за попытку подделать оценки");
+console.log(student.addMark(6,'geometry'));
