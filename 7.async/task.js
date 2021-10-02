19cfed8b89e70c20a54859deb164d98259b52617
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-10-03 01:41:47 +1100

    Async

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,14 +3,17 @@ class AlarmClock {
     this.alarmCollection = [];
     this.timerId = null;
   }
-  addClock(time, func, id){
-    if(id === undefined) {
-      throw new Error('error text')
-    }else if(this.timerId === id) {
-      console.error();
-    }else this.alarmCollection.push({time, func, id})
 
-  }
+  addClock(time, func, id) {
+    if (id === undefined) {
+      throw new Error('error text')
+    }
+    let alarm = 0;
+    this.alarmCollection.forEach(item => alarm = item.id)
+    if (alarm !== id) {
+      this.alarmCollection.push({time, func, id});
+    }else console.error("Error: Конфликт id")
+}
 
   removeClock(id){
     this.alarmCollection.filter((item) => {
@@ -19,49 +22,68 @@ class AlarmClock {
        return 'Звонок удален'
      }else return "НЕУДАЛОСЬ УДАЛИТЬ ЗВОНОК"
     })
-
     return this.alarmCollection;
-
   }
 
   getCurrentFormattedTime(){
    const data = new Date();
-   const Hour = data.getHours();
-   const Minutes = data.getMinutes();
-   return (Hour + ':' + Minutes)
+   let Hour = data.getHours();
+   let Minute = data.getMinutes();
+
+   Hour = (Hour < 10) ? '0' + Hour : Hour;
+   Minute = (Minute < 10) ? '0' + Minute : Minute;
+   return (Hour + ':' + Minute)
   }
 
-  start(){
-    let checkClock = ((addClock) => {
-     if(this.getCurrentFormattedTime() === addClock.time){
-
-     }
-    })
-    if(this.timerId === undefined){
-      this.timerId = setInterval(() => {
-        this.alarmCollection.forEach(checkClock)
-      })
-      return setInterval(this.timerId, 60000);
+  start() {
+    let checkClock = () => {
+      this.alarmCollection.filter(value => {
+        if(value.time === this.getCurrentFormattedTime()){
+      return value.func()
     }
-
-
+    })
+     }
+     const id = setInterval(checkClock, 60000)
+      if (Number.isNaN(this.timerId)) {
+        this.timerId = {id};
+      }
   }
 
   stop(){
-
+    let clearClock;
+    if (this.timerId > 0) {
+      clearInterval(this.timerId);
+    }
+      clearClock = this.alarmCollection.splice(0, this.alarmCollection.length)
+      return clearClock;
   }
 
   printAlarms(){
+      this.alarmCollection.forEach(value => console.log(value.id, value.time))
 
   }
   clearAlarms(){
+      clearInterval(this.timerId);
+      this.alarmCollection.splice(0, this.alarmCollection.length)
 
   }
 }
 
-const timer = new AlarmClock();
-timer.addClock('16:50', 2, 2);
-timer.removeClock(2)
-console.log(timer.getCurrentFormattedTime())
-console.log(timer.alarmCollection)
-console.log(timer.start())
+function testCase () {
+ const clock = new AlarmClock();
+ clock.addClock('01:39', f => console.log("Подъем"), 1);
+ clock.addClock('01:39', f => console.log("Подъем"), 2);
+ clock.addClock('01:39', f => console.log("Подъем"), 3);
+ clock.addClock('01:39', f => console.log("Подъем"), 4);
+ clock.addClock('01:39', f => console.log("Пора вставать"), 5);
+ clock.removeClock(2);
+ //clock.addClock('01:32', f => console.log("Иди умываться") );
+clock.addClock('01:40', f => console.log("Вставай а то проспишь"),6)
+ //clock.clearAlarms();
+   clock.printAlarms();
+ clock.addClock('01:40', f => console.log("Вставай а то проспишь"), 4);
+ clock.printAlarms();
+ clock.start()
+
+}
+console.log(testCase())
