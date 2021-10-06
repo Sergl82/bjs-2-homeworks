40fd30846740db337b10e1f730439c49f45081fa
Sergey <84701382+Sergl82@users.noreply.github.com>
2021-10-06 18:41:10 +1100

    Asyhc

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,21 @@
 // тут вы можете вызывать функции из task.js
+function testCase () {
+ const clock = new AlarmClock();
+ clock.addClock('18:41', () => console.log("Подъем"), 1);
+ clock.addClock('18:42', f => console.log("Подъем"), 2);
+ clock.addClock('20:07', f => console.log("Подъем"), 3);
+ clock.addClock('01:39', f => console.log("Подъем"), 4);
+ clock.addClock('01:39', f => console.log("Пора вставать"), 5);
+ clock.removeClock(2);
+ clock.addClock('01:32', f => console.log("Иди умываться") );
+clock.addClock('01:40', f => console.log("Вставай а то проспишь"),6)
+ clock.clearAlarms();
+  clock.printAlarms();
+ clock.addClock('01:40', f => console.log("Вставай а то проспишь"), 4);
+ clock.printAlarms();
+ clock.start()
+console.log(clock)
+ console.log(clock.getCurrentFormattedTime())
+}
+
+console.log(testCase())
--- 7.async/task.js
+++ 7.async/task.js
@@ -8,44 +8,44 @@ class AlarmClock {
     if (id === undefined) {
       throw new Error('error text')
     }
-    let alarm = 0;
-    this.alarmCollection.forEach(item => alarm = item.id)
-    if (alarm !== id) {
-      this.alarmCollection.push({time, func, id});
-    }else console.error("Error: Конфликт id")
-}
+    let alarms = this.alarmCollection.includes(id);
+    if(alarms !== true) {
+      this.alarmCollection.push({time, func, id})
+    }else
+    console.error("Error")
+  }
+
 
   removeClock(id){
-    this.alarmCollection.filter((item) => {
-     if (item.id === id){
-       this.alarmCollection.splice(item.id.length, 1);
+    let remove = this.alarmCollection.filter((item) => item.id !== id)
+     if (remove === true){
        return 'Звонок удален'
      }else return "НЕУДАЛОСЬ УДАЛИТЬ ЗВОНОК"
-    })
-    return this.alarmCollection;
-  }
+    }
+
 
   getCurrentFormattedTime(){
    const data = new Date();
-   let Hour = data.getHours();
-   let Minute = data.getMinutes();
+   let hour = data.getHours();
+   let minute = data.getMinutes();
 
-   Hour = (Hour < 10) ? '0' + Hour : Hour;
-   Minute = (Minute < 10) ? '0' + Minute : Minute;
-   return (Hour + ':' + Minute)
+   hour = (hour < 10) ? '0' + hour : hour;
+   minute = (minute < 10) ? '0' + minute : minute;
+   return (hour + ':' + minute)
   }
 
   start() {
-    let checkClock = () => {
-      this.alarmCollection.filter(value => {
-        if(value.time === this.getCurrentFormattedTime()){
-      return value.func()
+     let checkClock = () => {
+      this.alarmCollection.forEach(item => {
+        if (item.time === this.getCurrentFormattedTime()) {
+        return console.log(item.func())
+        }
+
+      })
     }
-    })
-     }
      const id = setInterval(checkClock, 60000)
       if (Number.isNaN(this.timerId)) {
-        this.timerId = {id};
+        this.timerId = id;
       }
   }
 
@@ -69,21 +69,4 @@ class AlarmClock {
   }
 }
 
-function testCase () {
- const clock = new AlarmClock();
- clock.addClock('01:39', f => console.log("Подъем"), 1);
- clock.addClock('01:39', f => console.log("Подъем"), 2);
- clock.addClock('01:39', f => console.log("Подъем"), 3);
- clock.addClock('01:39', f => console.log("Подъем"), 4);
- clock.addClock('01:39', f => console.log("Пора вставать"), 5);
- clock.removeClock(2);
- //clock.addClock('01:32', f => console.log("Иди умываться") );
-clock.addClock('01:40', f => console.log("Вставай а то проспишь"),6)
- //clock.clearAlarms();
-   clock.printAlarms();
- clock.addClock('01:40', f => console.log("Вставай а то проспишь"), 4);
- clock.printAlarms();
- clock.start()
 
-}
-console.log(testCase())
